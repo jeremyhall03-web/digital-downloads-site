bdc29ad229dfcceaa1fb393032e878a71c5a88bb
jeremyhall03-web <jeremyhall03@gmail.com>
2025-09-26 13:53:08 -0700

    Create astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+// Astro configuration file
+// See https://docs.astro.build/reference/config for more information.
+
+export default defineConfig({
+  site: 'https://example.com',
+  trailingSlash: 'never'
+});
